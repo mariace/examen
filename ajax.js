c2b37eab61102f15d18fa97e67f84afb757212b0
Celeste Fernandez <maria.celeste.fernandez.zuniga@intel.com>
2017-12-10 12:26:07 -0600

    change id in ajax.js

--- ajax.js
+++ ajax.js
@@ -2,7 +2,7 @@ function getPage(page){
 	var xHttpReq = new XMLHttpRequest();
 	xHttpReq.onreadystatechange = function(){
 		if(this.readyState == 4 && this.status == 200){
-			document.getElementById('viewer').innerHTML = this.responseText;
+			document.getElementById('main').innerHTML = this.responseText;
 			}
 	}
 		xHttpReq.open('GET', page,true);
