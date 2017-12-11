6398350c0e286991836e39b1cd57ee87e6efba8d
Celeste Fernandez <maria.celeste.fernandez.zuniga@intel.com>
2017-12-11 01:06:01 -0600

    complete

--- ajax.js
+++ ajax.js
@@ -8,3 +8,13 @@ function getPage(page){
 		xHttpReq.open('GET', page,true);
 		xHttpReq.send();
 }
+function getPage1(page){
+	var xHttpReq = new XMLHttpRequest();
+	xHttpReq.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			document.getElementById('wrapper').innerHTML = this.responseText;
+			}
+	}
+		xHttpReq.open('GET', page,true);
+		xHttpReq.send();
+}
